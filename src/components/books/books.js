be5c94cbcdd8c25680baadd8d394e39b5006db49
Francis Uloko <francisuloko@yahoo.com>
2021-09-03 22:11:12 +0100

    Refactor components

--- src/components/books/books.js
+++ src/components/books/books.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import hooks from '../../hooks/hooks';
 import Book from './book';
 
 const Books = () => {
   const books = useSelector((state) => state.books);
 
+  hooks();
+
   return (
     <ul>
       {books.map((book) => {
-        const { id, title, author } = book;
-        return <Book key={id} id={id} title={title} author={author} />;
+        <Book
+        key={book.item_id}
+        id={book.item_id}
+        title={book.title}
+        category={book.category}
+      />
       })}
     </ul>
   );
